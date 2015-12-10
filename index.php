84ba413fc0d67a2480579359a31b943010d713bb
Akira Uchihara <uchihara@tab.do>
2015-12-11 00:06:28 +0900

    Fix indent

--- index.php
+++ index.php
@@ -1,40 +1,3 @@
-<html>
-<head>
-<title>word finder</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-<script src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-<script language="javascript">
-$(document).ready(function() {
-  $("#reset").click(function(ev) {
-    $("#finder-form").find("textarea, :text, select").val("").end().find(":checked").prop("checked", false);
-  });
-});
-</script>
-<style type="text/css">
-  div#container {
-    margin: 0.5em;
-  }
-</style>
-</head>
-<body>
-<div id="container">
-<form id="finder-form" action="./index.php" method="get">
-<div id="pattern"><label for="pattern">pattern:</label><input type="text" name="pattern" value="<?= htmlspecialchars($_REQUEST["pattern"]) ?>"></div>
-<div id="uniq"><label for="uniq">uniq:</label><input type="checkbox" name="uniq" value="1"<?= $_REQUEST["uniq"]==1 ? "checked" : "" ?>></div>
-<div id="strings"><label for="strings">strings:</label><input type="text" name="strings" value="<?= htmlspecialchars($_REQUEST["strings"]) ?>"></div>
-<div id="length"><label for="length">length:</label>
-<select name="length">
-<?php for ($i=1; $i<=20; $i++) { ?>
-<option value="<?= $i ?>"<?= $_REQUEST["length"]==$i ? " selected" : ""?>><?= $i ?>
-<?php } ?>
-</select>
-</div>
-<div id="filters"><label for="filters">filters:</label><input type="text" name="filters" value="<?= htmlspecialchars($_REQUEST["filters"]) ?>"></div>
-<div id="reset"><input type="button" value="reset"></div>
-<div id="submit"><input type="submit" value="find"></div>
-</form>
 <?php
   function is_uniq($s) {
     $s1 = str_split($s);
@@ -47,15 +10,12 @@ $(document).ready(function() {
   if (!empty($_REQUEST["pattern"])) {
     $lines = file("./words.txt");
     $founds = preg_grep("/" . $_REQUEST["pattern"] . "/", $lines);
-?>
-    <ul id="results" data-role="listview" data-insert="true" data-filter="true">
-      <?php foreach ($founds as $found) { ?>
-        <?php if ($_REQUEST["uniq"]==false || is_uniq($found)) { ?>
-          <li class="result"><?= htmlspecialchars($found) ?></li>
-        <?php } ?>
-      <?php } ?>
-    </ul>
-<?php
+    $results = [];
+    foreach ($founds as $found) {
+      if ($_REQUEST["uniq"]==false || is_uniq($found)) {
+        $results[] = $result;
+      }
+    }
   }
 
   function parse_strings($s) {
@@ -91,16 +51,55 @@ $(document).ready(function() {
     $filters = empty($_REQUEST["filters"]) ? [] : explode(" ", $_REQUEST["filters"]);
     $counts = parse_strings($strings);
     $lines = file("./words.txt", FILE_IGNORE_NEW_LINES);
-?>
-    <ul id="results" data-role="listview" data-insert="true">
-      <?php foreach ($lines as $line) { ?>
-        <?php if (strlen($line)==$length && is_match($line, $counts) && !is_filtered($line, $filters)) { ?>
-          <li class="result"><?= htmlspecialchars($line) ?></li>
-        <?php } ?>
-      <?php } ?>
-    </ul>
-<?php
+    $results = [];
+    foreach ($lines as $line) {
+      if (strlen($line)==$length && is_match($line, $counts) && !is_filtered($line, $filters)) {
+        $results[] = $line;
+      }
+    }
   }
 ?>
-</div>
+<html>
+<head>
+  <title>word finder</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+  <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+  <style type="text/css">
+    div#container {
+      margin: 0.5em;
+    }
+  </style>
+</head>
+<body>
+  <div id="container">
+    <form id="finder-form" action="./index.php" method="get">
+      <div id="pattern"><label for="pattern">pattern:</label><input type="text" name="pattern" value="<?= htmlspecialchars($_REQUEST["pattern"]) ?>"></div>
+      <div id="uniq"><label for="uniq">uniq:</label><input type="checkbox" name="uniq" value="1"<?= $_REQUEST["uniq"]==1 ? "checked" : "" ?>></div>
+      <div id="strings"><label for="strings">strings:</label><input type="text" name="strings" value="<?= htmlspecialchars($_REQUEST["strings"]) ?>"></div>
+      <div id="length"><label for="length">length:</label>
+        <select name="length">
+          <?php for ($i=1; $i<=20; $i++) { ?>
+            <option value="<?= $i ?>"<?= $_REQUEST["length"]==$i ? " selected" : ""?>><?= $i ?></option>
+          <?php } ?>
+        </select>
+      </div>
+      <div id="filters"><label for="filters">filters:</label><input type="text" name="filters" value="<?= htmlspecialchars($_REQUEST["filters"]) ?>"></div>
+      <div id="reset"><input type="button" value="reset"></div>
+      <div id="submit"><input type="submit" value="find"></div>
+    </form>
+    <ul id="results" data-role="listview" data-insert="true">
+      <?php foreach ($results as $result) { ?>
+        <li class="result"><?= htmlspecialchars($found) ?></li>
+      <?php } ?>
+    </ul>
+  </div>
+  <script language="javascript">
+    $(document).ready(function() {
+      $("#reset").click(function(ev) {
+        $("#finder-form").find("textarea, :text, select").val("").end().find(":checked").prop("checked", false);
+      });
+    });
+  </script>
 </body>
