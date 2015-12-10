5faac810b62d41291d0ab7c56722a23af5d7063b
Akira Uchihara <uchihara@tab.do>
2015-12-10 18:49:44 +0900

    Fix checkbox checked

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
 <form action="./index.php" method="get">
 <div id="pattern"><span class="label">pattern:</span><input type="text" name="pattern" value="<?= htmlspecialchars($_REQUEST["pattern"]) ?>">
-<div id="uniq"><span class="label">uniq:</span><input type="checkbox" name="uniq" value="1" "<?= $_REQUEST["uniq"]==1 ? "checked" : "" ?>">
+<div id="uniq"><span class="label">uniq:</span><input type="checkbox" name="uniq" value="1"<?= $_REQUEST["uniq"]==1 ? "checked" : "" ?>>
 <input type="submit" value="find">
 </form>
 <?php
