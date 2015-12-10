015117f06081ce40e11e2a930f806eac2edd7308
Akira Uchihara <uchihara@tab.do>
2015-12-10 18:05:59 +0900

    form追加

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Hello Heroku World!";
+var_dump($_REQUEST);
 ?>
+<html>
+<head>
+<title>word finder</title>
+</head>
+<body>
+<form action="./index.php" method="get">
+<div id="pattern"><span class="label">pattern:</span><input type="text" name="pattern" value="<?= htmlspecialchars($_REQUEST["pattern"]) ?>">
+<div id="uniq"><span class="label">uniq:</span><input type="checkbox" name="uniq" value="1" "<?= $_REQUEST["uniq"]==1 ? "checked" : "" ?>">
+<input type="submit" value="find">
+</form>
+</body>
