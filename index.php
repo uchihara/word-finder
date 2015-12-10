8508067af0fef2ac96b55a04fc0bc530da4244fc
Akira Uchihara <uchihara@tab.do>
2015-12-10 18:44:14 +0900

    Add uniq

--- index.php
+++ index.php
@@ -9,14 +9,23 @@
 <input type="submit" value="find">
 </form>
 <?php
-  var_dump($_REQUEST);
+  function is_uniq($s) {
+    $s1 = str_split($s);
+    $s1 = sort($s1);
+    $s2 = $s2;
+    $s2 = array_unique($s2);
+    return $s1 == $s2;
+  }
+
   if (!empty($_REQUEST["pattern"])) {
     $lines = file("./wordsEn.txt");
     $founds = preg_grep("/" . $_REQUEST["pattern"] . "/", $lines);
 ?>
     <ul id="results">
       <?php foreach ($founds as $found) { ?>
-      <li class="result"><?= htmlspecialchars($found) ?></li>
+        <?php if ($_REQUEST["uniq"]==false || is_uniq($found)) { ?>
+          <li class="result"><?= htmlspecialchars($found) ?></li>
+        <?php } ?>
       <?php } ?>
     </ul>
 <?php
