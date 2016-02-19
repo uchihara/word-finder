4e7926e4c9a4ab676c6687aa0f34ae18938473e3
Akira Uchihara <uchihara@tab.do>
2016-02-19 19:55:27 +0900

    Fix typo

--- app.rb
+++ app.rb
@@ -4,11 +4,10 @@ require 'sinatra/reloader'
 set :erb, :trim => '-'
 
 get '/' do
-  p params
   texts = params['texts'] || ''
   matrix = texts.split(/,/).map{|row|row.split(//)}
-  lenghts = (params['lengths'] || []).map(&:to_i)
+  lengths = (params['lengths'] || []).map(&:to_i)
   nouse_dict = params['nouse_dict']=='1'
   results = []
-  erb :index, locals: { texts: texts, lengths: lenghts, nouse_dict: nouse_dict, results: results }
+  erb :index, locals: { texts: texts, lengths: lengths, nouse_dict: nouse_dict, results: results }
 end
