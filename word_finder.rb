41e56769c0b4bb89993e1249cd2f4c5fd4f5daaf
Akira Uchihara <uchihara@tab.do>
2016-02-19 19:40:17 +0900

    Change to word_finder class

---
+++ word_finder.rb
@@ -0,0 +1,30 @@
+require './matrix'
+require './string_scanner'
+require './dict'
+
+class WordFinder
+  def initialize matrix, lengths, use_dict
+    @matrix = Matrix.new matrix
+    @lengths = lengths
+    @use_dict = use_dict
+  end
+
+  def find_words
+    scanner = StringScanner.new @matrix
+    results = Set.new
+    (0...@matrix.x_max).each do |x|
+      (0...@matrix.y_max).each do |y|
+        @lengths.each do |length|
+          dict = Dict.new(length)
+          hits = scanner.scan(length, x, y)
+          if @use_dict
+            results.merge hits & dict.dict
+          end
+        end
+      end
+    end
+    results.sort.to_a
+  end
+end
+
+
