7dab7caa388aa7c2abfd86c089b14745eb111aed
Akira Uchihara <uchihara@tab.do>
2016-02-19 23:52:55 +0900

    Enable easy input

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ require_relative 'models/word_finder'
 set :erb, :trim => '-'
 
 get '/' do
-  texts = params['texts'] || ''
-  matrix = texts.strip.gsub(/\r/, "").split(/\n+/).map{|row|row.split(//)}
+  texts = (params['texts'] || '').downcase.gsub(/[ \r]/, "").gsub(/\n+/, "\n")
+  matrix = texts.split(/\n/).map{|row|row.split(//)}
   lengths = (params['lengths'] || []).map(&:to_i)
   nouse_dict = params['nouse_dict']=='1'
   if texts && !lengths.empty?
