ddc5a0e1b7d70c2252b45066ceda1f92da0cb3d0
Akira Uchihara <uchihara@tab.do>
2016-02-19 19:32:06 +0900

    Change php to dummy sinatra app

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+set :erb, :trim => '-'
+
 get '/' do
-  erb :index
+  p params
+  texts = params['texts'] || ''
+  matrix = texts.split(/,/).map{|row|row.split(//)}
+  lenghts = (params['lengths'] || []).map(&:to_i)
+  nouse_dict = params['nouse_dict']=='1'
+  results = []
+  erb :index, locals: { texts: texts, lengths: lenghts, nouse_dict: nouse_dict, results: results }
 end
